d6e065ddf61297f78b3e36309ebd03d2c32b289b
Davide Barchetta <dbarchetta@gheppio.hq.mquadro.net>
2022-10-18 09:35:12 +0200

    Aggiunta configurazione mouse
    Configurazione che serve per i tasti della mia trackball Kensington SlimBlade

---
+++ mouse.sh
@@ -0,0 +1,16 @@
+#xinput set-prop "Logitech USB Receiver Mouse" "Coordinate Transformation Matrix" -0.4 0 0 0 0.4 0 0 0 1
+#xinput set-prop "Logitech USB Receiver Mouse" "libinput Accel Speed" 0
+
+xinput set-int-prop "Kensington Slimblade Trackball" "Evdev Middle Button Emulation" 8 1
+
+xinput set-int-prop "Kensington Slimblade Trackball" "Evdev Middle Button Emulation" 8 0 7 8 9
+
+#xinput set-button-map "Kensington Slimblade Trackball" 1 8 2 4 5 6 7 3
+xinput set-button-map "Kensington Slimblade Trackball" 1 8 3 4 5 6 7 2
+
+xinput set-int-prop "Kensington Slimblade Trackball" "Evdev Wheel Emulation" 8 1
+xinput set-int-prop "Kensington Slimblade Trackball" "Evdev Wheel Emulation Button" 8 8
+xinput set-int-prop "Kensington Slimblade Trackball" "Evdev Wheel Emulation Axes" 8 6 7 4 5
+xinput set-int-prop "Kensington Slimblade Trackball" "Evdev Wheel Emulation Timeout" 16 300
+#xinput set-prop "Kensington Slimblade Trackball" "libinput Drag Lock Buttons" 8
+#xinput set-prop "Kensington Slimblade Trackball" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
